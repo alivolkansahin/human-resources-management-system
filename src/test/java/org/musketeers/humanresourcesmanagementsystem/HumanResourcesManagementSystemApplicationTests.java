d936d999e5caabc0b7dbbc0ed34236be73e0402e
alivolkansahin <avolkan.shn@gmail.com>
2024-02-06 00:14:42 +0300

    First Commit

---
+++ src/test/java/org/musketeers/humanresourcesmanagementsystem/HumanResourcesManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.musketeers.humanresourcesmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

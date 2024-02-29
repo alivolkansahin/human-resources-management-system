382bcd6f5bd572b0969353df9a9c845e482d5b68
alivolkansahin <avolkan.shn@gmail.com>
2024-02-29 04:18:14 +0300

    Config service and config git services have been deployed and everything worked as expected. Gateway service is still work in progress...

---
+++ gateway-service/src/main/java/org/musketeers/constant/Endpoint.java
@@ -0,0 +1,30 @@
+package org.musketeers.constant;
+
+
+public abstract class Endpoint {
+
+    public static final String FALLBACK = "/fallback";
+
+    public static final String ADMIN = "/admin";
+
+    public static final String ADVANCE = "/advance";
+
+    public static final String AUTH = "/auth";
+
+    public static final String COMMENT = "/comment";
+
+    public static final String COMPANY = "/company";
+
+    public static final String DAY_OFF = "/day-off";
+
+    public static final String GUEST = "/guest";
+
+    public static final String MAIL = "/mail";
+
+    public static final String PERSONNEL = "/personnel";
+
+    public static final String SPENDING = "/spending";
+
+    public static final String SUPERVISOR = "/supervisor";
+
+}
